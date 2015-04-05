45d1fb00e30407039c2ea501f645e8a9cf37c38d
Eric Ouyang <contact@ericouyang.com>
2015-04-05 13:19:57 -0400

    customizations for cdb site

--- simple_community/urls.py
+++ simple_community/urls.py
@@ -4,7 +4,7 @@ from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
 
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, RedirectView
 
 handler403 = 'simple_community.views.permission_denied'
 
@@ -16,6 +16,7 @@ urlpatterns = patterns('',
 
     url(r'^admin/', include(admin.site.urls)),
     url(r'^directory/', include('directory.urls')),
+    url(r'^accounts/signup/$', RedirectView.as_view(url='/')),
     url(r'^accounts/', include('allauth.urls'))
 ) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) \
   + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
