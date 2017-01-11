4b8bc4fda9820632dc8ca110c10dd19908249c11
Eric Ouyang <contact@ericouyang.com>
2017-01-10 20:55:39 -0500

    integrate flatpages

--- simple_community/urls.py
+++ simple_community/urls.py
@@ -1,10 +1,11 @@
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.contrib.flatpages import views
 
 from django.conf import settings
 from django.conf.urls.static import static
 
-from django.views.generic import TemplateView
+from .views import HomePageView
 
 handler403 = 'simple_community.views.permission_denied'
 
@@ -12,10 +13,11 @@ urlpatterns = [
     # Examples:
     # url(r'^$', 'simple_community.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-    url(r'^$', TemplateView.as_view(template_name='pages/home.haml')),
+    url(r'^$', HomePageView.as_view()),
 
     url(r'^admin/', include(admin.site.urls)),
     url(r'^directory/', include('directory.urls')),
-    url(r'^accounts/', include('allauth.urls'))
+    url(r'^accounts/', include('allauth.urls')),
+    url(r'^(?P<url>.*/)$', views.flatpage),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) \
   + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- simple_community/views.py
+++ simple_community/views.py
@@ -1,9 +1,29 @@
 from django.template import RequestContext
 from django.shortcuts import render_to_response
+from django.views.generic.base import TemplateView
 
+from django.contrib.flatpages.models import FlatPage
 
 def permission_denied(request):
     return render_to_response(
         'errors/permission_denied',
         context_instance=RequestContext(request)
     )
+
+class HomePageView(TemplateView):
+    template_name = "pages/home.haml"
+
+    def get_context_data(self, **kwargs):
+        context = super(HomePageView, self).get_context_data(**kwargs)
+        try:
+            fp = FlatPage.objects.get(url='/')
+            context['title'] = fp.title
+            context['content'] = fp.content
+        except Exception:
+            context['title'] = 'Welcome to Simple Community'
+            context['content'] = "It's simply a community"
+        return context
+
+    
+
+    
